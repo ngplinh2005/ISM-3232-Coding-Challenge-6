a01f457c5bb7d808577e924fe678dc59232f2065
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-09-22 14:24:16 -0400

    Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -15,3 +15,24 @@ function displayEmployeeShifts(employee) {
         console.log(`${shift.day}: ${shift.hours} hours`)
     })
 }
+
+// Task 3: Create a Function to Assign a New Shift
+
+function assignShift(employeeName, day, hours) {
+    const employee = employees.find(employee => employee.name === employeeName)
+
+    if (!employee) {
+        console.log(`Employee ${employeeName} is not found.`)
+        return
+    }
+
+    for (let i = 0; i < employee.shifts.length; i++) {
+        if (employee.shifts[i].day === day) {
+            console.log(`${employeeName} is already assigned a shift for ${day}.`)
+            return
+        }
+    }
+
+    employee.shifts.push({ day, hours})
+    console.log(`${employeeName} has been assigned a ${hours} shift on ${day}.`)
+}
